971794ad9608861584bbedabcd9c4754602f6c2a
Tobias Blazor <tobias1153@gmail.com>
2024-03-18 01:28:30 +0100

    login und logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {AuthenticationGuard} from './authentication.guard'
+import {UnauthenticatedGuard} from "./unauthenticated.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent, canActivate: [AuthenticationGuard] },
+  { path: 'login', component: LoginPageComponent, canActivate: [UnauthenticatedGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { environment } from '../environments/environment';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
