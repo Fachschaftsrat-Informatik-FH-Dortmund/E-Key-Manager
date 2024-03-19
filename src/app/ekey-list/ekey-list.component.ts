0249f7a880f7dce039922ab1ef705e949661d460
Tobias Blazor <tobias1153@gmail.com>
2024-03-19 17:39:11 +0100

    ekey component, homepage -> dashbboard

---
+++ src/app/ekey-list/ekey-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ekey-list',
+  templateUrl: './ekey-list.component.html',
+  styleUrl: './ekey-list.component.css'
+})
+export class EkeyListComponent {
+
+}
