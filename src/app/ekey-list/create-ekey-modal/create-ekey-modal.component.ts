d347e497f98710025d56a931416de17bd3d748c9
Tobias Blazor <tobias1153@gmail.com>
2024-03-19 22:45:10 +0100

    create ekey modal

--- src/app/ekey-list/create-ekey-modal/create-ekey-modal.component.ts
+++ src/app/ekey-list/create-ekey-modal/create-ekey-modal.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, TemplateRef } from '@angular/core';
+
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {Ekey} from "../ekey.model";
 
 @Component({
   selector: 'app-create-ekey-modal',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './create-ekey-modal.component.css'
 })
 export class CreateEkeyModalComponent {
+  private modalService = inject(NgbModal);
+  protected ekey:Ekey;
+
+  open(content: TemplateRef<any>) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' })
+  }
+
+  createEkey() {
+
+    this.modalService.dismissAll();
+  }
+
+
 
 }
