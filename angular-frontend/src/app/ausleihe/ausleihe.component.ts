b4435ae3bd99ed2c696c925cafa03da1dd8439de
Jan Schneider <jan@schneider-mail.net>
2024-03-19 21:05:08 +0100

    student ausleihe form jetzt template

--- angular-frontend/src/app/ausleihe/ausleihe.component.ts
+++ angular-frontend/src/app/ausleihe/ausleihe.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Student } from '../../models/student.model';
 
 @Component({
   selector: 'app-ausleihe',
   templateUrl: './ausleihe.component.html',
-  styleUrl: './ausleihe.component.css'
+  styleUrl: './ausleihe.component.css',
 })
 export class AusleiheComponent {
+  model = new Student(72, '', '', '');
 
+  studentsubmitted = false;
+
+  onSubmit() {
+
+    this.studentsubmitted = true;
+  }
 }
