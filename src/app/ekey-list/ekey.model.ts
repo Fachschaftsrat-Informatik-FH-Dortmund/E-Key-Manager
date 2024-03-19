7c2d1ff6d97aeb81852c92d1a8430fb22b050488
Tobias Blazor <tobias1153@gmail.com>
2024-03-19 17:38:48 +0100

    ekey model

---
+++ src/app/ekey-list/ekey.model.ts
@@ -0,0 +1,31 @@
+export class Ekey {
+  public ekeyID: string;
+  public besitzer: string;
+  public status: string;
+  public berechtigung: string;
+  public notiz: string;
+
+  constructor(ekeyID: string, status: string, besitzer: string, berechtigung: string, notiz: string) {
+    this.ekeyID = ekeyID;
+
+    if(besitzer == "FSR" || besitzer == "ausgeliehen" || besitzer == "verloren") {
+      this.besitzer = besitzer;
+    }else {
+      throw new Error("Ungültiger Besitzer. Besitzer muss FSR, ausgeliehen oder verloren sein!");
+    }
+
+    if(status=="defekt" || status=="gesperrt" || status=="funktioiert") {
+      this.status =  status;
+    }else {
+      throw new Error("Ungültiger Status. Status muss defekt, gesperrt oder funktioniert sein!");
+    }
+
+    if(berechtigung=="STUD" || berechtigung=="FSRF" || "FSR") {
+      this.berechtigung = berechtigung;
+    }else {
+      throw new Error("Ungültige Berechtigung. Berechtigung muss STUD, FSRF oder FSR sein!");
+    }
+
+    this.notiz = notiz;
+  }
+}
