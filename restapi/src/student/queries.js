4af0935c36a1c4e2f27de71ce90789898c6d4834
Tobias Blazor <tobias1153@gmail.com>
2024-03-21 21:58:08 +0100

    SQL geschrieben

--- restapi/src/student/queries.js
+++ restapi/src/student/queries.js
@@ -1,9 +1,9 @@
 // abfragen
 const getStudenten = "SELECT * FROM student";
-const getStudent = "SELECT * FROM student WHERE mat = $1";
-const addStudent = "INSERT INTO ekey( ekeyid, besitzer, zustand, berechtigung, notiz) VALUES($1, $2, $3, $4, $5)";
-const deleteStudent = "DELETE FROM ekey WHERE ekeyid = $1";
-const updateStudent = "UPDATE ekey SET besitzer=$2, zustand=$3, berechtigung=$4, notiz=$5 WHERE ekeyid=$1"
+const getStudent = "SELECT * FROM student WHERE matrnr = $1";
+const addStudent = "INSERT INTO student(matrnr, vorname, nachname, email, hat_Studienbescheinigung) VALUES($1, $2, $3, $4, $5)";
+const deleteStudent = "DELETE FROM student WHERE matrnr = $1";
+const updateStudent = "UPDATE student SET vorname=$2, nachname=$3, email=$4, hat_Studienbescheinigung=$5 WHERE matrnr=$1"
 
 module.exports = {
   getStudenten,
