3287562bc283e4c1c67a9a65280ab9b35a135fed
Tobias Blazor <tobias1153@gmail.com>
2024-03-22 21:37:04 +0100

    guard entfernt

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {LoginPageComponent} from "./login-page/login-page.component";
-import {AuthenticationGuard} from './authentication.guard'
-import {UnauthenticatedGuard} from "./unauthenticated.guard";
 import {AusleiheComponent} from "./ausleihe/ausleihe.component";
 import {EkeyListComponent} from "./ekey-list/ekey-list.component";
 import { RueckgabeComponent} from "./rueckgabe/rueckgabe.component";
 import {RueckmeldungComponent} from "./rueckmeldung/rueckmeldung.component";
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent, canActivate: [AuthenticationGuard] },
-  { path: 'login', component: LoginPageComponent, canActivate: [UnauthenticatedGuard] },
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginPageComponent },
   { path: 'ausleihe', component: AusleiheComponent },
   { path: 'ekey-list', component: EkeyListComponent },
   { path: 'rückgabe', component: RueckgabeComponent},
