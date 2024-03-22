832bccdacd916826e1766bb77a9652076e8a1700
Jan Schneider <jan@schneider-mail.net>
2024-03-22 12:26:14 +0100

    keine unerlaubten ausleihen möglich

--- angular-frontend/src/app/ausleihe/ausleihe.component.ts
+++ angular-frontend/src/app/ausleihe/ausleihe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Student } from '../../models/student.model';
 import {Ausleihe} from "../../models/ausleihe.model";
@@ -18,26 +18,34 @@ export class AusleiheComponent {
   student =new Student(72)
   ekey=new Ekey('','funktioniert','Student','STUD','');
   ausleihe = new Ausleihe(0,this.student.MatrNr,this.ekey.ekeyid,new Date(),true);
+  ekeys: Ekey[]= [];
 
-  ausl: Observable<Ausleihe[]> | undefined;
-  step=0;
+  step=1;
+  count : number =0;
   onStudentsubmit() {
-
-    this.ausl=this.http.get<Ausleihe[]>(this.ROOT_URL+"ausleihen?matrnr="+this.student.MatrNr)
-
-    this.ausl.subscribe(value => console.log('Observable emitted the next value: ' + value[0]))
-
-    if(Object.keys(this.http.get(this.ROOT_URL+"ausleihen?matrnr="+this.student.MatrNr)).length>0){
-      console.log("möp")
-    }
-
-    this.step++;
+    this.http.get<Ausleihe[]>("http://localhost:3000/api/v1/ausleihen?matrnr="+this.student.MatrNr).subscribe({next: (l)=> {
+        if (l.length == 0) {
+          this.step++;
+        } else {
+          console.log("Dieser Student besitzt bereits einen E-Key aktiv")
+          //TODO: Error behandlugn wenn schon ein Key ausgeliehen ist
+        }
+      }}
+    )
   }
 
   onKeySumbmit(){
-    //TODO: Prüfen ob key echt ist
+
+    this.http.get<Ekey[]>("http://localhost:3000/api/v1/ekeys/"+this.ekey.ekeyid).subscribe({next: (l)=> {
+        if (l.length >0 ) {
+          this.step++;
+        } else {
+          console.log("dieser E-Key existiert nicht")
+          //TODO: Error behandlugn wenn so ein key nicht existiert
+        }
+      }}
+    )
     //TODO: PDF erstellen
-    this.step++;
   }
 
   submit(){
