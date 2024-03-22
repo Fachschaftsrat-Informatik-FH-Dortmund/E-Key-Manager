dd66989a0f631ff97a9c60e4557fd64ba8e8d9a7
Tobias Blazor <tobias1153@gmail.com>
2024-03-22 23:36:35 +0100

    Loading spinner und logik

--- angular-frontend/src/app/auth/auth.component.ts
+++ angular-frontend/src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {Router} from "@angular/router";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +9,61 @@ import { Component } from '@angular/core';
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  isLoginMode = true;
+  isLoading= false;
+  errorMessageMap: any = {
+    'auth/invalid-email': 'Email ungültig',
+    'auth/invalid-credential': 'Falsches Passwort oder Account existiert nicht',
+    'auth/missing-password': 'Passwort fehlt'
+  }
+  error: string = '';
+  constructor(private auth: AngularFireAuth, private router: Router) { }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form:NgForm) {
+    if(!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password
+
+    this.isLoading=true;
+    if(this.isLoginMode) {
+      this.auth.signInWithEmailAndPassword(email, password)
+        .then(() =>
+          this.isLoading=false
+        )
+        .catch(error => {
+          for (let errorType of Object.keys(this.errorMessageMap)) {
+            if (error.message.includes(errorType)) {
+              this.isLoading=false;
+              this.error = this.errorMessageMap[errorType];
+              break;
+            }
+          }
+        });
+
+    }else {
+      this.auth.createUserWithEmailAndPassword(email, password)
+        .then(() =>
+          this.isLoading=false
+        )
+        .catch(error => {
+          for (let errorType of Object.keys(this.errorMessageMap)) {
+            if (error.message.includes(errorType)) {
+              this.isLoading=false;
+              this.error = this.errorMessageMap[errorType];
+              break;
+            }
+          }
+        });
+    }
+
+    form.reset();
+  }
 
 }
