2570166bb2389bd66fd0fe4f8be7e4d5e8856eab
Tobias Blazor <tobias1153@gmail.com>
2024-03-23 02:00:35 +0100

    fix

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {AusleiheComponent} from "./ausleihe/ausleihe.component";
 import {EkeyListComponent} from "./ekey-list/ekey-list.component";
 import { RueckgabeComponent} from "./rueckgabe/rueckgabe.component";
 import {RueckmeldungComponent} from "./rueckmeldung/rueckmeldung.component";
-import { AngularFireAuthGuard, hasCustomClaim, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
 import {AuthComponent} from "./auth/auth.component";
 
 //Gute Doku für Guards https://github.com/angular/angularfire/blob/master/site/src/auth/route-guards.md
@@ -13,12 +13,12 @@ const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['auth']);
 const redirectLoggedInToRoot = () => redirectLoggedInTo(['/']);
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
-  { path: 'ausleihe', component: AusleiheComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
-  { path: 'ekey-list', component: EkeyListComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
-  { path: 'rückgabe', component: RueckgabeComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
-  { path: 'rückmeldung', component: RueckmeldungComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
-  { path: 'auth', component: AuthComponent}
+  { path: '',             component: DashboardComponent,    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'ausleihe',     component: AusleiheComponent,     canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
+  { path: 'ekey-list',    component: EkeyListComponent,     canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
+  { path: 'rückgabe',     component: RueckgabeComponent,    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'rückmeldung',  component: RueckmeldungComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'auth',         component: AuthComponent,         canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLoggedInToRoot }}
 ];
 
 @NgModule({
