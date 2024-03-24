cbd5f562314ee76b7103658c069e20f7b21f7736
Jan Schneider <jan@schneider-mail.net>
2024-03-24 13:32:26 +0100

    erweiterte ekey api count mit /count und filtern

--- restapi/src/ekey/routes.js
+++ restapi/src/ekey/routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const controller = require('./controller');
 
-const router = Router();
+const router = Router()
 
-router.get('/', controller.getEkeys);
+router.get('/', controller.getEkeysdata);
+router.get('/count', controller.getEkeyscount);
 router.post('/', controller.addEkey)
 router.post('/sperren', controller.sperreEkey)
 router.get('/:ekeyID', controller.getEkeyById);
