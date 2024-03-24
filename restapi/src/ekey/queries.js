708c75b3eb3404ef8eaa277da9e5cf408aa7f4d1
Jan Schneider <jan@schneider-mail.net>
2024-03-24 23:11:42 +0100

    rücknahmen sind möglich

--- restapi/src/ekey/queries.js
+++ restapi/src/ekey/queries.js
@@ -6,6 +6,7 @@ const deleteEkeyById = "DELETE FROM ekey WHERE ekeyid = $1";
 const updateEkey = "UPDATE ekey SET besitzer=$2, zustand=$3, berechtigung=$4, notiz=$5 WHERE ekeyid=$1"
 const sperreEkey = "SELECT keysperren($1)"
 const getUebersicht="SELECT  * FROM ekey WHERE besitzer LIKE $1 AND zustand LIKE $2 AND berechtigung LIKE $3;"
+const zuruechnehmen ="SELECT keyzurueck($1)"
 
 module.exports = {
   getEkeys,
@@ -14,5 +15,6 @@ module.exports = {
   deleteEkeyById,
   updateEkey,
   sperreEkey,
-  getUebersicht
+  getUebersicht,
+  zuruechnehmen
 }
--- restapi/src/ekey/routes.js
+++ restapi/src/ekey/routes.js
@@ -7,6 +7,8 @@ router.get('/', controller.getEkeysdata);
 router.get('/count', controller.getEkeyscount);
 router.post('/', controller.addEkey)
 router.post('/sperren', controller.sperreEkey)
+//TODO: router.post('/entsperren', controller.sperreEkey)
+router.post('/zuruecknehmen', controller.zurueckEkey)
 router.get('/:ekeyID', controller.getEkeyById);
 router.delete('/:ekeyID', controller.deleteEkeyById);
 router.put('/', controller.updateEkey);
