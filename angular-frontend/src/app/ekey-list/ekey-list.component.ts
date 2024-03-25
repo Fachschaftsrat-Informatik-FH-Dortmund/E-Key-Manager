52b9a6fa8ed08f262f1146c2dd5f615b3fa62e53
Jan Schneider <jan@schneider-mail.net>
2024-03-25 03:05:37 +0100

    auth bei ekey

--- angular-frontend/src/app/ekey-list/ekey-list.component.ts
+++ angular-frontend/src/app/ekey-list/ekey-list.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import { Ekey } from "../../models/ekey.model";
-import { HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { Observable} from "rxjs";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-ekey-list',
@@ -11,9 +12,25 @@ import { Observable} from "rxjs";
 export class EkeyListComponent implements OnInit{
   readonly ROOT_URL = 'http://localhost:3000/api/v1/ekeys/'
   ekeys: Observable<Ekey[]> | undefined;
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
-    this.ekeys = this.http.get<Ekey[]>(this.ROOT_URL)
+  constructor(private http: HttpClient,public afAuth: AngularFireAuth) {
   }
+
+  uid:string="";
+  ngOnInit() {
+    this.afAuth.authState.subscribe(async user => {
+      if (user) {
+        // Obtain the Firebase ID token
+        const idToken = await user.getIdToken(true);
+        console.log('User is signed in, UID:', user.uid);
+        // Send the ID token in the Authorization header
+        const headers = new HttpHeaders().set('Authorization', idToken);
+        console.log(headers.get("Authorization"))
+        this.ekeys= this.http.get<Ekey[]>(this.ROOT_URL, { headers });
+
+      } else {
+        // Handle the case where the user is not signed in
+      }
+    });
+  }
+
 }
--- restapi/server.js
+++ restapi/server.js
@@ -8,14 +8,38 @@ var cors = require('cors')
 const app = express();
 const port = 3000;
 
+
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+ async function checkauth (req, res, next)  {
+  const idToken = req.headers.authorization; // Assuming the token is sent in the Authorization header
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken; // Attach the decoded token to the request object
+    next(); // Proceed to the next middleware or route handler
+  } catch (error) {
+    res.status(401).send('Unauthorized');
+  }
+};
+
 app.use(cors());
+app.use(checkauth)
+
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 })
 
 app.use('/api/v1/ekeys', ekeyRoutes);
-app.use('/api/v1/studenten', studentRoutes);
+app.use('/studenten', studentRoutes);
 app.use('/api/v1/ausleihen', ausleihRoutes);
 app.use('/api/v1/kasse', kassenRoutes);
 app.listen(port, () => console.log(`App listening on Port ${port}`));
