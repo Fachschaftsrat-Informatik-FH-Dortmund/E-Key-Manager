b4b7bab9e81c960dc9478006eaa677e7dc1d5cfa
Jan Schneider <39964221+graylayy@users.noreply.github.com>
2024-03-25 12:28:24 +0100

    Update restapi/auth.js
    Co-authored-by: Tobi <37587918+tobi-blazor@users.noreply.github.com>

--- restapi/auth.js
+++ restapi/auth.js
@@ -5,7 +5,6 @@ async function checkauth (req, res, next)  {
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
     req.user = decodedToken; // Attach the decoded token to the request object
-    console.log("yippie")
     next(); // Proceed to the next middleware or route handler
   } catch (error) {
     res.status(401).send('Unauthorized');
