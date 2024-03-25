d5d2a6f9f8cdc4d9e4770d3b87aa7cdb37cb9539
Tobi <37587918+tobi-blazor@users.noreply.github.com>
2024-03-25 09:35:30 +0100

    Update restapi/src/ekey/queries.js

--- restapi/src/ekey/queries.js
+++ restapi/src/ekey/queries.js
@@ -6,7 +6,7 @@ const deleteEkeyById = "DELETE FROM ekey WHERE ekeyid = $1";
 const updateEkey = "UPDATE ekey SET besitzer=$2, zustand=$3, berechtigung=$4, notiz=$5 WHERE ekeyid=$1"
 const sperreEkey = "SELECT keysperren($1)"
 const getUebersicht="SELECT  * FROM ekey WHERE besitzer LIKE $1 AND zustand LIKE $2 AND berechtigung LIKE $3;"
-const zuruechnehmen ="SELECT keyzurueck($1)"
+const zuruecknehmen ="SELECT keyzurueck($1)"
 
 module.exports = {
   getEkeys,
