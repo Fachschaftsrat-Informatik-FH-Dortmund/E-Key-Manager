cc989b45d53b0f7f7e6a0fc7a43e66ea81de7713
Jan Schneider <39964221+graylayy@users.noreply.github.com>
2024-03-25 12:28:41 +0100

    Update angular-frontend/src/app/ekey-list/ekey-list.component.ts
    Co-authored-by: Tobi <37587918+tobi-blazor@users.noreply.github.com>

--- angular-frontend/src/app/ekey-list/ekey-list.component.ts
+++ angular-frontend/src/app/ekey-list/ekey-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { Ekey } from "../../models/ekey.model";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable} from "rxjs";
 
 @Component({
